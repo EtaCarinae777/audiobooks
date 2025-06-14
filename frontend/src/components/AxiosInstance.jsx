56883100f22c3a9446c409f16cd93ee036dffcac
EtaCarinae777 <128357960+EtaCarinae777@users.noreply.github.com>
2025-06-14 16:22:57 +0200

    stylowo

--- frontend/src/components/AxiosInstance.jsx
+++ frontend/src/components/AxiosInstance.jsx
@@ -1,46 +1,39 @@
-import axios from 'axios';
+import axios from 'axios'
 
 const baseUrl = 'http://127.0.0.1:8000/'
 
 const AxiosInstance = axios.create({
-    baseURL : baseUrl, 
-    timeout : 5000,
-    headers : {
-        "Content-Type": "application/json",
-        accept : "application/json",
-
+    baseURL: baseUrl,
+    timeout: 5000, 
+    headers:{
+        "Content-Type":"application/json",
+         accept: "application/json"
     }
 })
 
-// Automatycznie dodaj token do każdego zapytania
 AxiosInstance.interceptors.request.use(
     (config) => {
-        const token = localStorage.getItem('Token');
-        if (token) {
+        const token = localStorage.getItem('Token')
+        if(token){
             config.headers.Authorization = `Token ${token}`
         }
-        else ( // Jeśli nie ma tokena, to nie dodawaj go
+        else{
             config.headers.Authorization = ``
-        )
+        }
         return config;
     }
-    // },
-    // (error) => {
-    //     return Promise.reject(error);
-    // }
-);
+)
 
-// Automatycznie wyloguj gdy token wygaśnie
 AxiosInstance.interceptors.response.use(
-    (response) => response,
+    (response) => {
+        return response
+    }, 
     (error) => {
-        // Jeśli serwer mówi "nie masz uprawnień" - wyloguj
-        if (error.response && error.response.status === 401) { //401 - Unauthorized/iNVALID
+        if(error.response && error.response.status === 401){
             localStorage.removeItem('Token')
-            window.location.href = '/'
         }
-        return Promise.reject(error)
+
     }
-);
+)
 
 export default AxiosInstance;
